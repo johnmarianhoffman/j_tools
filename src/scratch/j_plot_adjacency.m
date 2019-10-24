0a5b87c5958beb2fce0ea71c5012151c16e1facf
John Hoffman <jhoffman@magicleap.com>
2019-10-23 19:51:09 -0700

    buttcheeks

---
+++ src/scratch/j_plot_adjacency.m
@@ -0,0 +1,17 @@
+function j_plot_adjacency(points,adjacency_mat)
+
+    lines = zeros(0,2);
+    
+    for i=1:size(adjacency_mat,1)
+        for j=1:size(adjacency_mat,2)
+            if adjacency_mat(i,j)
+                p1 = points(i,:);
+                p2 = points(j,:);
+                lines(end+1:end+3,:) = [p1;p2;nan(1,2)];
+            end
+        end
+    end
+
+    line(lines(:,1),lines(:,2));
+
+end
